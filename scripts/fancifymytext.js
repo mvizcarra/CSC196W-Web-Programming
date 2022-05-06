70982b07069e3418714853272f146a2aaf5a63f4
Michael Vizcarra <50968946+mvizcarra@users.noreply.github.com>
2022-05-06 12:03:30 -0700

    Assignment 4

--- scripts/fancifymytext.js
+++ scripts/fancifymytext.js
@@ -1,3 +1,29 @@
-function myFunc(){
-    alert("Hello, world!");
+function biggerFunc(){
+    // alert("Hello, world!");
+    document.getElementById("userText").style.fontSize = "4em";
+
 }
+
+function fancyFunc(){
+    document.getElementById("userText").style.fontFamily = "Brush Script MT";
+    document.getElementById("userText").style.fontWeight = "Bold";
+    document.getElementById("userText").style.color= "blue";
+    document.getElementById("userText").style.textDecoration = "underline";
+}
+
+function boringFunc(){
+    document.getElementById("userText").style.fontFamily = "";
+    document.getElementById("userText").style.fontWeight = "";
+    document.getElementById("userText").style.color= "";
+    document.getElementById("userText").style.textDecoration = "";
+}
+
+function mooFunc(){
+    document.getElementById("userText").style.textTransform = "uppercase";
+    var str = document.getElementById("userText").value;
+    var parts = str.split(".");
+    str = parts.join(" moo.");
+    document.getElementById("userText").value = str;
+
+}
+
